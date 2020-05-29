a6dc58c7ce92918c2a36890caa6aa74fae2f73c3
Navtosh <navtosh2001@gmail.com>
2020-05-29 13:49:15 +0530

    sdafs

--- Searcha2Dmatrix.cpp
+++ Searcha2Dmatrix.cpp
@@ -31,23 +31,25 @@ bool Search2DMatrix2(int mat[], int n, int m, int k){
     while (left <= right) { 
         int mid = left + (right - left) / 2; 
   
-        // if element found return true 
-        if (mat[mid] == k) 
+        // if element found print "Found"
+        // and return true
+        if (mat[mid] == k){
+        	cout << "Found";
             return true; 
+        }
   
-        // if middle less than K then 
-        // skip the left part of the 
-        // array else skip the right part 
+        // if mid element is less than check in right part
+        // else check in left part
         if (mat[mid] < k) 
             left = mid + 1; 
         else
             right = mid - 1;
     } 
   
-    // if not found return false 
-    return false; 
-
-
+    // if 'k' is not found then print "Not Found"
+    // and return false
+    cout << "Not Found";
+    return false;
 }
 
 
@@ -57,20 +59,17 @@ bool SearchRow(int** mat, int n, int m, int k){
     int right = m - 1; 
     while (left <= right) { 
         int mid = left + (right - left) / 2; 
-  
-        // if the element lies in the range 
-        // of this row then call 
-        // 1-D binary search on this row 
+  		// check if this row may contain 'k'
+  		// if this condition holds true
+  		// binary search the row to find 'k'
         if (k >= mat[mid][0] 
             && k <= mat[mid][n - 1]) 
             return Search2DMatrix2(mat[mid], n, m, k); 
   
-        // if the element is less then the 
-        // starting element of that row then 
-        // search in upper rows else search 
-        // in the lefter rows 
+        // if element is less then, search in upper rows
+        // else search in lower row
         if (k < mat[mid][0]) 
-            right = mid - 1; 
+            right = mid - 1;
         else
             left = mid + 1; 
     } 
