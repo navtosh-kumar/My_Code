588f016cda849ecd36408d2f504dc1cc0f6a3654
Navtosh <navtosh2001@gmail.com>
2020-05-29 13:50:48 +0530

    asdf

--- Searcha2Dmatrix.cpp
+++ Searcha2Dmatrix.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 void Search2DMatrix1(int** mat, int n, int m, int k){
 
-
+	// code to traverse the whole matrix
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
+			// if the element is found
 			if(mat[i][j] == k){
 				cout << "Found";
 				return;
 			}
 		}
 	}
-
+	// if the element is not found
 	cout << "Not Found";
 	return;
 
